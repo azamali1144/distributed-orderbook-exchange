92a09c815438fd5b4c7d5e49924d0dcd406a9223
azam <muhammad.azam@simplexts.biz>
2026-03-19 22:31:22 +0500

    chore: add configuration and apply it

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 'use strict'
 
-const OrderBookPeer = require('./src/peer');
+require('dotenv').config()
+const OrderBookPeer = require('./src/peer')
+const { DEFAULT_GRAPE } = require('./src/constants')
 
-const GRAPE_URL = 'http://127.0.0.1:30001';
-
-// In a real scenario, we'd use different ports for different local instances
-const rpcPort = 10000 + Math.floor(Math.random() * 1000);
-const node = new OrderBookPeer({
-    grape: GRAPE_URL,
-    rpcPort: rpcPort
-});
-
-try {
-    node.init()
-    console.log(`Orderbook Node started on RPC port: ${rpcPort}`);
-} catch (err) {
-    console.error('Failed to start node:', err);
+const config = {
+    grape: process.env.GRAPE_URL || DEFAULT_GRAPE,
+    rpcPort: parseInt(process.env.BASE_RPC_PORT) + Math.floor(Math.random() * 1000)
 }
+
+const node = new OrderBookPeer(config)
+node.init()
--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,11 @@
 'use strict'
 
 module.exports = {
-    ORDER_BOOK_RPC: 'order_book_rpc',
-    ORDER_BOOK_GOSSIP: 'order_book_gossip'
-};
+    ORDER_BOOK_RPC: 'order_book_api',
+    ORDER_BOOK_GOSSIP: 'order_book_gossip_topic',
+    DEFAULT_GRAPE: 'http://127.0.0.1:30001',
+    SIDES: {
+        BUY: 'buy',
+        SELL: 'sell'
+    }
+}
